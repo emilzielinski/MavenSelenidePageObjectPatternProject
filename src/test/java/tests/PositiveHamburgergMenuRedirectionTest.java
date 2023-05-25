8fb41dc7a11da624c5b6f9563b3e53aac295bb3f
EM <emil0900@gmail.com>
2023-05-25 15:55:33 +0200

    PositiveHamburgergMenuRedirectionTest test tweaked and additional assertion added.

--- src/test/java/tests/PositiveHamburgergMenuRedirectionTest.java
+++ src/test/java/tests/PositiveHamburgergMenuRedirectionTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.testng.annotations.Test;
 import page.objects.ProductsPage;
 
+import static com.codeborne.selenide.CollectionCondition.size;
 import static com.codeborne.selenide.Selenide.$;
 
 public class PositiveHamburgergMenuRedirectionTest extends PositiveLoginTest {
@@ -17,13 +18,15 @@ public class PositiveHamburgergMenuRedirectionTest extends PositiveLoginTest {
         ProductsPage productsPage = new ProductsPage();
         productsPage.hamburgerMenuButton.click();
 
-        int size = productsPage.table.size();
-        System.out.println(size);
+        /*Assertion*/
+        productsPage.table.shouldHave(size(4));
+        /*End of assertion*/
 
         productsPage.aboutSection.click();
 
         /*Assertion*/
         $(By.xpath("//*[@src='/images/logo.svg']")).shouldBe(Condition.visible);
+        /*End of assertion*/
 
 
     }
